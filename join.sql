86614fb661e1a797869c22937b5ac9dcee06724b
Luis Octavio <luis_ram1997@hotmail.com>
2020-05-20 17:26:10 -0500

    finish tutorial 6

--- join.sql
+++ join.sql
@@ -20,4 +20,6 @@ SELECT stadium, COUNT(teamid) FROM game JOIN goal ON (matchid = id) GROUP BY sta
 
 SELECT matchid,mdate,COUNT(teamid) FROM game JOIN goal ON matchid = id WHERE (team1 = 'POL' OR team2 = 'POL') GROUP BY matchid;
 
-SELECT matchid, mdate, COUNT(teamid) FROM game JOIN goal ON (matchid = id) WHERE teamid = 'GER' GROUP BY id
+SELECT matchid, mdate, COUNT(teamid) FROM game JOIN goal ON (matchid = id) WHERE teamid = 'GER' GROUP BY id;
+
+SELECT mdate, team1, SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1, team2, SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2 FROM game LEFT JOIN goal ON matchid = id GROUP BY mdate, matchid;
