758ed765473ebf97cfc4fd7aa1ee8077f05e065b
Luis Octavio <luis_ram1997@hotmail.com>
2020-05-20 17:25:45 -0500

    finish tutorial 6

---
+++ join.sql
@@ -0,0 +1,23 @@
+SELECT matchid, player FROM goal WHERE teamid = 'GER';
+
+SELECT id,stadium,team1,team2 FROM game WHERE id = 1012;
+
+SELECT player,teamid, stadium, mdate FROM game JOIN goal ON (id=matchid) WHERE teamid = 'GER';
+
+SELECT team1, team2, player FROM game JOIN goal ON (id=matchid) WHERE player LIKE 'Mario%';
+
+SELECT player ,teamid ,coach , gtime FROM goal JOIN eteam ON (teamid = id) WHERE gtime<=10;
+
+SELECT mdate, teamname FROM game JOIN eteam ON (team1 = eteam.id) WHERE coach = 'Fernando Santos';
+
+SELECT player FROM goal JOIN game ON (id = matchid) WHERE stadium = 'National Stadium, Warsaw';
+
+SELECT DISTINCT player FROM game JOIN goal ON matchid = id  WHERE teamid != 'GER' AND (team1 = 'GER' XOR team2 = 'GER');
+
+SELECT teamname, COUNT(teamid) FROM eteam JOIN goal ON id=teamid GROUP BY teamname;
+
+SELECT stadium, COUNT(teamid) FROM game JOIN goal ON (matchid = id) GROUP BY stadium;
+
+SELECT matchid,mdate,COUNT(teamid) FROM game JOIN goal ON matchid = id WHERE (team1 = 'POL' OR team2 = 'POL') GROUP BY matchid;
+
+SELECT matchid, mdate, COUNT(teamid) FROM game JOIN goal ON (matchid = id) WHERE teamid = 'GER' GROUP BY id
