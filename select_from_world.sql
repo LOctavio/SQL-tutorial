7147a5985bd3c1d5ccefd227363aca5c7c44410e
Luis Octavio <luis_ram1997@hotmail.com>
2020-05-20 14:59:56 -0500

    use xor operator

--- select_from_world.sql
+++ select_from_world.sql
@@ -14,7 +14,7 @@ SELECT name FROM world WHERE name LIKE '%United%';
 
 SELECT name, population, area FROM world WHERE area > 3000000 OR population > 250000000;
 
-SELECT name, population, area FROM world WHERE (area > 3000000 AND NOT(population > 250000000)) OR (NOT(area > 3000000) AND population > 250000000);
+SELECT name, population, area FROM world WHERE area > 3000000 XOR population > 250000000
 
 SELECT name, ROUND(population/1000000,2) AS population, ROUND(gdp/1000000000, 2) AS gdp FROM world WHERE continent = 'South America'
 
